df684888b282a2226fa3c6fadd5cddb860b26f34
Jasen Lew <jasen.lew@gmail.com>
2014-08-16 16:39:23 -0700

    Adds Near Me button and send location information to map

--- campaignCents/ViewController.swift
+++ campaignCents/ViewController.swift
@@ -7,21 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Creates the variables that will be passed onto MapVC
     var latSelected:Double = 0.0
     var lngSelected:Double = 0.0
-    
     var deltaSelected:Double = 0.0
-
+    
+    var manager:CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        // NOTE: requestWhenInUseAuthorization is only available on iOS 8
+        // manager.requestWhenInUseAuthorization()
+        
+        manager.startUpdatingLocation()
         
         // Hides navigationController on first VC, which is loaded on app load
         self.navigationController.navigationBarHidden = true
     }
     
+    // The "National" button that sets the location information before segueing to map
     @IBAction func national(sender: AnyObject) {
         // Geographic center of the contiguous United States: http://en.wikipedia.org/wiki/Geographic_center_of_the_contiguous_United_States
         latSelected = 39.50
@@ -31,6 +43,22 @@ class ViewController: UIViewController {
         deltaSelected = 60.0
     }
     
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        // NOTE: By default, will be an object, so need to cast to CLLocation
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Prints in console the object of the user's location information
+        println("JASEN|userLocation: \(userLocation)")
+        
+        // NOTE: Use string interpolation to convert CLLocationDegrees type to String type
+        latSelected = userLocation.coordinate.latitude
+        lngSelected = userLocation.coordinate.longitude
+        
+        deltaSelected = 0.21
+    }
+    
+    // The "Near Me" button that retrieves the user location information before segueing to map
     @IBAction func nearMe(sender: AnyObject) {
         println("Yo")
     }
@@ -50,5 +78,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
