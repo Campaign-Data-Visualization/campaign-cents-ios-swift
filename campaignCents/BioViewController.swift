fba865e7cf59fdd9b1dc4a2f12233359fcda6dc6
Jasen Lew <jasen.lew@gmail.com>
2014-08-18 22:05:50 -0700

    Adds bioVC

---
+++ campaignCents/BioViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BioViewController.swift
+//  campaignCents
+//
+//  Created by Jasen Lew on 8/18/14.
+//  Copyright (c) 2014 Jasen Lew. All rights reserved.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
