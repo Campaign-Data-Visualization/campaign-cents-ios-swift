abcbace43b79de8a8a1ffe9b47a3fd374238d1ad
Jasen Lew <jasen.lew@gmail.com>
2014-08-22 20:09:37 -0700

    WIP launch animation

--- campaignCents/AppDelegate.swift
+++ campaignCents/AppDelegate.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import QuartzCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-                            
-    var window: UIWindow?
 
+    var window: UIWindow?
+/*    var mask: CALayer?
+    var imageView: UIImageView?
+*/
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+/*        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+        let imageView = UIImageView(frame: self.window!.frame)
+        imageView.image = UIImage(named: "twitterscreen")
+        self.window!.addSubview(imageView)
+        
+        // Where the logo is added
+        self.mask = CALayer()
+        self.mask!.contents = UIImage(named: "twitter logo mask").CGImage
+        self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.mask!.position = CGPoint(x: imageView.frame.size.width/2, y: imageView.frame.size.height/2)
+        imageView.layer.mask = mask
+        self.imageView = imageView
+        
+        animateMask()
+        
         // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor(red: 70/255, green: 154/255, blue: 233/255, alpha: 1)
+        self.window!.makeKeyAndVisible()
+        UIApplication.sharedApplication().statusBarHidden = true
+*/
         return true
     }
 
@@ -39,7 +63,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication!) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+/*
+    // Animate logo fade in-and-out
+    func animateMask() {
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 1 // Number of seconds the animation lasts
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 2 // Number of seconds before animation begins
+        let initalBounds = NSValue(CGRect: mask!.bounds)
+        let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 90))
+        let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 1500, height: 1500))
+        keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+        self.mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
+    }
+    
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        self.imageView!.layer.mask = nil // Removes logo mask when animation completes
+//        self.window!.rootViewController = UINavigationController()
+    }
+*/
 }
 
