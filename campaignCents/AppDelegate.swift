8850e1a273b0cc33b6e2fd584f8f1cdbafd9e99d
Jasen Lew <jasen.lew@gmail.com>
2014-08-23 22:38:28 -0700

    Adds twitter animation

--- campaignCents/AppDelegate.swift
+++ campaignCents/AppDelegate.swift
@@ -11,17 +11,24 @@ import QuartzCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-/*    var mask: CALayer?
+    var mask: CALayer?
     var imageView: UIImageView?
-*/
+    
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
-/*        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-
+        
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+//        self.window = application.windows.first as? UIWindow
+        
         let imageView = UIImageView(frame: self.window!.frame)
         imageView.image = UIImage(named: "twitterscreen")
+        
         self.window!.addSubview(imageView)
+        
+        
         
         // Where the logo is added
         self.mask = CALayer()
@@ -32,38 +39,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         imageView.layer.mask = mask
         self.imageView = imageView
         
-        animateMask()
         
+        
+        animateMask()
         // Override point for customization after application launch.
-        self.window!.backgroundColor = UIColor(red: 70/255, green: 154/255, blue: 233/255, alpha: 1)
-        self.window!.makeKeyAndVisible()
+        //self.window!.backgroundColor = UIColor(red: 70/255, green: 154/255, blue: 233/255, alpha: 1)
+//        self.window!.makeKeyAndVisible()
         UIApplication.sharedApplication().statusBarHidden = true
-*/
+
+
+        
+        
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication!) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication!) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication!) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication!) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication!) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-/*
+    
+    
     // Animate logo fade in-and-out
     func animateMask() {
         let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
@@ -81,8 +93,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
         self.imageView!.layer.mask = nil // Removes logo mask when animation completes
-//        self.window!.rootViewController = UINavigationController()
+        self.imageView?.removeFromSuperview()
+        
+        self.window!.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as UIViewController
+        
     }
-*/
+    
 }
 
